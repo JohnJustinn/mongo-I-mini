d17f8258f2613a5a8560a2ba3dcfb6e610d06203
Johnathan Justinn <jjustinn85@gmail.com>
2018-02-20 16:16:42 -0500

    API up and running

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const helmet = require('helmet');
-const cors = require('cors'); // https://www.npmjs.com/package/cors
+const cors = require('cors'); 
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 
 const server = express();
 
-server.use(helmet()); // https://helmetjs.github.io/
-server.use(cors());   // https://medium.com/trisfera/using-cors-in-express-cac7e29b005b
+server.use(helmet()); 
+server.use(cors());   
 server.use(bodyParser.json());
 
 server.get('/', function(req, res) {
   res.status(200).json({ status: 'API Running' });
 });
+
+mongoose
+      .connect('mongodb://localhost/BearKeeper')
+      .then(db => {
+        console.log(`Successfully connect to the ${db.connections[0].name} database`);
+      })
+      .catch(error => {
+        console.log(`Database Connection Failed`);
+      });
 
 const port = process.env.PORT || 5005;
 server.listen(port, () => {
